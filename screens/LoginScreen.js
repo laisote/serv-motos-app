24e169b93908cbb01f90b1af9f073e041939f2cc
Lais Moreira <laisdsmoreira@usp.br>
2024-10-10 23:17:30 -0300

    Implementação da tela de login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,62 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
 
 export default function LoginScreen() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+
+    const handleLogin = () => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                console.log('Usuário logado:', userCredential.user);
+                // Redirecionar ou fazer algo após o login
+            })
+            .catch((error) => {
+                setErrorMessage(error.message);
+            });
+    };
+
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={StyleSheet.container}>
+            <TextInput
+                styles={styles.input}
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+            />
+            <TextInput
+                styles={styles.input}
+                placeholder="Senha"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+            />
+            {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+            <Button title="Login" onPress={handleLogin} />
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+    },
+    input: {
+        height: 50,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 20,
+        paddingHorizontal: 10,
+    },
+    errorText: {
+        color: 'red',
+        marginBottom: 10,
+    },
+});
